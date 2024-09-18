051cc7c6735a56ab55cb659d74efe68bc47b846a
Petr Kekalo <kekalo.petr@gmail.com>
2024-09-18 01:03:41 -0400

    API fetch completed

--- js/index.js
+++ js/index.js
@@ -36,3 +36,33 @@ for (let i = 0; i < skills.length; i++) {
 
      skillsList.appendChild(skill);
 }
+ 
+const githubUsername = 'pkekalo';
+const url = `https://api.github.com/users/${githubUsername}/repos`;
+
+const projectSection = document.getElementById('projects');
+const projectList = projectSection.querySelector('ul');
+
+fetch(url)
+  .then(response => response.json())
+  .then(repositories => {
+    console.log(repositories);
+
+    if (repositories.length === 0) {
+      console.log('No repositories found.');
+      projectSection.innerHTML = '<p>No repositories available.</p>';
+      return;
+    }
+
+    repositories.forEach(repo => {
+      const project = document.createElement('li');
+      
+      project.innerText = repo.name;
+
+      projectList.appendChild(project);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching repositories:', error);
+    projectSection.innerHTML = '<p>There was an error fetching repositories.</p>';
+  });
